1fe470193586a942a81f9daf2100dc7065361b1a
lausc <lauri.schnabel05@gmail.com>
2023-04-22 19:26:36 +0200

    Abstract eingefügt

--- src/main/java/com/itestra/domain/AbstractDomainModel.java
+++ src/main/java/com/itestra/domain/AbstractDomainModel.java
@@ -4,8 +4,8 @@ import io.quarkus.mongodb.panache.PanacheMongoEntity;
 
 import java.util.UUID;
 
-public class AbstractDomainModel extends PanacheMongoEntity {
-    private String uuid;;
+public abstract class AbstractDomainModel extends PanacheMongoEntity {
+    private String uuid;
 
     public AbstractDomainModel() {
         this.uuid = UUID.randomUUID().toString();
--- src/main/java/com/itestra/domain/Game.java
+++ src/main/java/com/itestra/domain/Game.java
@@ -1,11 +1,12 @@
 package com.itestra.domain;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class Game extends AbstractDomainModel {
 
-    private LocalDate date;
+    private LocalDateTime date;
     private List<String> playerId;
 
     public Game(String uuid) {
